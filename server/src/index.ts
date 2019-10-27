72de3beb7642447821de6bda7efa0a11dc3b2a82
Hugo de Melo <hsemog@gmail.com>
2019-10-27 07:50:37 +0100

    setup cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,15 @@ import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
 import cookieParser from "cookie-parser";
 import { refreshToken } from "./Auth";
+import cors from "cors";
 
 (async () => {
     const app = express();
+
+    app.use(cors({
+        credentials: true,
+        origin: "http://localhost:3000"
+    }));
     app.use(cookieParser());
 
     await createConnection();
@@ -23,7 +29,7 @@ import { refreshToken } from "./Auth";
         context: ({ req, res }) => ({ req, res })
     });
 
-    apolloServer.applyMiddleware({ app: app });
+    apolloServer.applyMiddleware({ app: app, cors: false });
 
     app.listen(4000, () => console.log("http://localhost:4000/graphql"));
 })();
